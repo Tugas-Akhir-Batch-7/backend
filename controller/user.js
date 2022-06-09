d1d637e2e474eddd252dfe36bfac145a9649458e
dhiaaziz <azizwkwkwk@gmail.com>
2022-06-09 19:09:39 +0700

    login

--- controller/user.js
+++ controller/user.js
@@ -1,13 +1,13 @@
-const jwb = require("jsonwebtoken")
+// const jwb = require("jsonwebtoken")
 const bcrypt = require('bcrypt')
 const mv = require('mv')
-const {QueryTypes} = require('sequelize')
+const { QueryTypes } = require('sequelize')
 
 const db = require('../db/models')
-const {mail, mailOptions} = require('../model/mail');
+const { mail, mailOptions } = require('../model/mail');
 const { sequelize } = require("../db/models");
-// const errorHandler = require('../helpers/errorHandler');
-const APIError = require('../helpers/api-error');
+const ApiError = require('../helpers/api-error');
+const { generateToken, verify } = require("../helpers/jwt-auth")
 //db
 // const sequelize = db.index
 const user = db.User
@@ -20,18 +20,47 @@ const SU = {
     password: 'faishalSample__07'
 }
 
-class User{
-    static async login(req, res){
+class User {
+    static async login(req, res, next) {
         try {
-            const isi = await user.findAll()
-            console.log('jalan')
-            res.json(isi)
+            const { email, password } = req.body;
+            const userGet = await user.findOne({
+                where: {
+                    email: email
+                }
+            });
+            if (userGet) {
+                // console.log(password)
+                const isPassword = bcrypt.compareSync(password, userGet.password);
+                // console.log(isPassword)
+                if (isPassword) {
+                    userGet.password = undefined;
+                    const payload = {
+                        id: userGet.id,
+                        name: userGet.name,
+                        email: userGet.email,
+                        role: userGet.role,
+                    }
+                    const token = generateToken(payload);
+                    // const cobaVerify = verify(token)
+
+                    return res.status(200).json({
+                        success: true,
+                        message: 'login berhasil',
+                        data: userGet,
+                        token: token,
+                        // cobaVerify
+                    })
+                }
+            }
+            throw ApiError.badRequest('Username or password is incorrect');
         } catch (error) {
-            console.log(error)
-            res.status(400).send('terjadi error')
+            next(error)
+            // console.log(error)
+            // res.status(400).send('terjadi error')
         }
     }
-    static async register(req, res){
+    static async register(req, res) {
         try {
             //variabel
             const name = req.body.username
@@ -40,37 +69,37 @@ class User{
             const role = req.body.role
             const inOtp = req.body.otp
             let photo
-            
+
             //cek
-            if(!(name && password && email && role && inOtp)) throw 'masukkan semua data'
-            
+            if (!(name && password && email && role && inOtp)) throw 'masukkan semua data'
+
             //image
-            if(req.file){ //jika memasukkan photo profile
+            if (req.file) { //jika memasukkan photo profile
                 photo = req.file.filename
-                if(!(req.file && /jpeg|jpg|png/.test(req.file.mimetype))) throw 'masukkan photo'
+                if (!(req.file && /jpeg|jpg|png/.test(req.file.mimetype))) throw 'masukkan photo'
                 mv(
-                    `./public/-/${photo}`, 
-                    `./public/img-profile/${photo}`, 
-                    {clobber: false}, 
+                    `./public/-/${photo}`,
+                    `./public/img-profile/${photo}`,
+                    { clobber: false },
                     (err) => { if (err) throw 'gagal menyimpan gambar' }
                 )
-            }else{
+            } else {
                 photo = 'default.png'
             }
 
             //cek otp
-            let otp = await otpRegistrasi.findOne({where:{otp: inOtp}})
-            if(!otp) throw 'otp tidak valid'
+            let otp = await otpRegistrasi.findOne({ where: { otp: inOtp } })
+            if (!otp) throw 'otp tidak valid'
 
             //cek data
-            if(new Date() > otp.valid_until) throw 'waktu otp limit'
-            if(!(email == otp.email)) throw 'data tidak lengkap'
+            if (new Date() > otp.valid_until) throw 'waktu otp limit'
+            if (!(email == otp.email)) throw 'data tidak lengkap'
 
             //kirim data register ke database
-            if(role == 'murid'){
-                await user.create({name, email, password, role, email_verified_at: new Date(), photo})
-            }else{
-                await user.create({name, email, password, role, photo})
+            if (role == 'murid') {
+                await user.create({ name, email, password, role, email_verified_at: new Date(), photo })
+            } else {
+                await user.create({ name, email, password, role, photo })
             }
 
             res.send('register berhasil')
@@ -79,65 +108,65 @@ class User{
             res.status(400).json(['terjadi error', error])
         }
     }
-    static async createOtpRegister(req, res){
+    static async createOtpRegister(req, res) {
         try {
             //variabel
             let emailOtp
             const email = req.body.email
             const role = req.body.role
             const usernameAdmin = req.body.usernameAdmin
-            
+
             //cek email
-            if(!(email && role)) throw 'lengkapi semua data'
+            if (!(email && role)) throw 'lengkapi semua data'
 
             //role
-            if(role == 'admin'){
+            if (role == 'admin') {
                 emailOtp = SU.email
-            }else if(role == 'guru'){
+            } else if (role == 'guru') {
                 //cek penerima otp
-                if(!usernameAdmin) throw 'masukkan username admin untuk menerima otp'
+                if (!usernameAdmin) throw 'masukkan username admin untuk menerima otp'
                 //mencari email penerima
                 emailOtp = await user.findOne({
                     attributes: ['email'],
-                    where: {name: usernameAdmin, role: 'admin'}
+                    where: { name: usernameAdmin, role: 'admin' }
                 })
                 //cek adat tidaknya username penerima email otp
-                if(!emailOtp) throw 'username tidak ada'
+                if (!emailOtp) throw 'username tidak ada'
                 emailOtp = emailOtp.email
-            }else if(role == 'murid'){
+            } else if (role == 'murid') {
                 //cek penerima otp
-                if(!req.body.email) throw 'masukkan email'
+                if (!req.body.email) throw 'masukkan email'
                 emailOtp = req.body.email
-            }else{
+            } else {
                 throw 'role tidak tersedia'
             }
 
             //otp
             let otp = 0
-            while(otp < 100000 || otp > 999999){
+            while (otp < 100000 || otp > 999999) {
                 otp = Math.ceil(Math.random() * 1000000)
             }
 
             await sequelize.query(`INSERT INTO "otp_registrasi" 
                 ("email", "otp", "valid_until", "updated_at", "created_at")
-                VALUES ('${email}', '${otp}', '${new Date(new Date().getTime() + (1000*60* timeOtp)).toISOString()}', '${new Date().toISOString()}', '${new Date().toISOString()}')
+                VALUES ('${email}', '${otp}', '${new Date(new Date().getTime() + (1000 * 60 * timeOtp)).toISOString()}', '${new Date().toISOString()}', '${new Date().toISOString()}')
                 ON CONFLICT ("email")
                 DO UPDATE SET
                     "email" = EXCLUDED."email",
                     "otp" = EXCLUDED."otp",
                     "valid_until" = EXCLUDED.valid_until,
                     "updated_at" = EXCLUDED.updated_at;`,
-              {
-                // replacements: [email, otp, new Date(new Date().getTime() + (1000*60* timeOtp)), new Date()],
-                type: QueryTypes.UPSERT
-              }
+                {
+                    // replacements: [email, otp, new Date(new Date().getTime() + (1000*60* timeOtp)), new Date()],
+                    type: QueryTypes.UPSERT
+                }
             )
 
             //kirim otp via email
             mailOptions.to = emailOtp
             mailOptions.text = `code otp\n${otp}`
-            mail.sendMail(mailOptions, (err, info)=>{
-                if(err){
+            mail.sendMail(mailOptions, (err, info) => {
+                if (err) {
                     console.log(err)
                     throw 'error saat kirim otp via email'
                 }
@@ -149,14 +178,14 @@ class User{
         }
     }
 
-    static async profile(req, res, next){
+    static async profile(req, res, next) {
         try {
             // console.log(await user.findAll()
             // const id = req.params.id
             const userGet = await user.findByPk(1)
 
-            if(!userGet) throw APIError.badRequest("User tidak ditemukan")
-            
+            if (!userGet) throw ApiError.badRequest("User tidak ditemukan")
+
             userGet.password = undefined
             return res.status(200).json({
                 success: true,
@@ -165,7 +194,7 @@ class User{
             })
             // console.log("All users:", JSON.stringify(users, null, 2));
             res.send(req.params.id)
-        } catch(error) {
+        } catch (error) {
             next(error)
             // console.log(error)
             // res.status(400).send('terjadi error')
