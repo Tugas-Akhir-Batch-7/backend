47a223cc8e4f518fd659cbbfd0255564ea52af04
dhiaaziz <azizwkwkwk@gmail.com>
2022-06-09 14:57:39 +0700

    error handler middlewares and helpers

---
+++ db/models/otp.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Otp extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Otp.init({
+    user_id: DataTypes.INTEGER,
+    valid_until: DataTypes.DATE,
+    otp: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Otp',
+    underscored: true,
+    tableName: 'otp'
+  });
+  return Otp;
+};
